8ce05e8b9e7a789d1e615236601991213566e657
yanis <yanis@fetch.fr>
2021-07-07 11:46:19 +0200

    Add truncate filter, KnpTimeBundle and php.ini file for timezone

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Fidry\PsyshBundle\PsyshBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
