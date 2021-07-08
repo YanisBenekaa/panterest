7c8bf66fc24e2d5daa9072e2c29a647e17ad6b47
yanis <yanis@fetch.fr>
2021-07-08 12:29:05 +0200

    Add image input in form and fix vich_uploader.yaml error

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -6,12 +6,19 @@ use App\Entity\Pin;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class PinType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image (JPG or PNG file)',
+                'required' => false,
+                'allow_delete' => true,
+                'download_uri' => false,
+            ])
             ->add('title')
             ->add('description')
         ;
