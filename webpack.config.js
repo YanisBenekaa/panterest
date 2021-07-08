cc7154c430a2530d19fcd3f613a38fca7ba328fe
yanis <yanis@fetch.fr>
2021-07-08 17:21:20 +0200

    Copy files from assets/images to public/build via Webpack and style homepage

--- webpack.config.js
+++ webpack.config.js
@@ -72,6 +72,12 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    .copyFiles({
+        from: './assets/images',
+        to: '[path][name].[hash:8].[ext]',
+        context: './assets'
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
