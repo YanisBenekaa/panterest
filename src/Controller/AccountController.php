49e7e6e5071f06d8fcfd06e3b8a57e8cd576bcf0
yanis <yanis@fetch.fr>
2021-07-16 09:30:49 +0200

    Add edit account feature

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use MercurySeries\FlashyBundle\FlashyNotifier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/account", name="app_account")
+     * @Route("/account", name="app_account", methods={"GET"})
      */
     public function show(): Response
     {
         return $this->render('account/show.html.twig');
+    }
+
+    /**
+     * @Route("/account/edit", name="app_account_edit", methods={"GET", "POST", "PUT"})
+     */
+    public function edit(Request $request, EntityManagerInterface $em, FlashyNotifier $flashy): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $flashy->success('Account successful edited !');
+
+            return $this->redirectToRoute('app_account');
+        }
+
+        return $this->render('account/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/UserFormType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
